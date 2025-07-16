22dd962d9383964927640d1cd794988be279136c
Ravaillack <matkowski.lucas@gmail.com>
2025-07-16 13:15:57 +0200

    mariadb en cours

---
+++ srcs/requirements/mariadb/conf/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mysqld safe &
+sleep 10
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}`"
+
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USER}\`@'%';"
+
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+mariadb -e "FLUSH PRIVILEGES;"
+
+mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+killall mysqld_safe
+wait
+
+exec mysqld_safe
